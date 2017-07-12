101fc39cd305a842eaf8481ab82f2601b8915a80
padarshr <padarshr@codeaurora.org>
2017-07-12 16:46:43 +0530

    land: Update vndk-sp build mechanism and library set.
    This change is to add some more libraries needed to
    be present in the vndk-sp library set. Also, move towards
    a better the mechanism to build vndk-sp.

--- device.mk
+++ device.mk
@@ -337,6 +337,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
 
+# VNDK-SP:
+PRODUCT_PACKAGES += \
+    vndk-sp
+
 # Whitelisted app
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
