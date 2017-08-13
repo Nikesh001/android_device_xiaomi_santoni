d66b8d9002c163f9ff0bc346befd510189c71ead
bishnuprasad884uu <bishnuprasad884uu@gmail.com>
2017-08-13 12:46:28 +0530

    Revert changes

--- Android.mk
+++ Android.mk
@@ -38,25 +38,19 @@ $(LOCAL_BUILT_MODULE): WCNSS_BIN_SYMLINK := $(TARGET_OUT)/etc/firmware/wlan/prim
 $(LOCAL_BUILT_MODULE): ACTUAL_DAT_FILE := /persist/WCNSS_wlan_dictionary.dat
 $(LOCAL_BUILT_MODULE): WCNSS_DAT_SYMLINK := $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_wlan_dictionary.dat
 
-$(LOCAL_BUILT_MODULE): ACTUAL_MODULE_FILE := /system/lib/modules/pronto/pronto_wlan.ko
-$(LOCAL_BUILT_MODULE): WCNSS_MODULE_SYMLINK := $(TARGET_OUT)/lib/modules/wlan.ko
-
- $(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/Android.mk
- $(LOCAL_BUILT_MODULE):
- 	$(hide) echo "Making symlinks for wifi"
- 	$(hide) mkdir -p $(dir $@)
- 	$(hide) mkdir -p $(dir $(WCNSS_INI_SYMLINK))
-	$(hide) mkdir -p $(dir $(WCNSS_MODULE_SYMLINK))
- 	$(hide) rm -rf $@
- 	$(hide) rm -rf $(WCNSS_INI_SYMLINK)
- 	$(hide) ln -sf $(ACTUAL_INI_FILE) $(WCNSS_INI_SYMLINK)
- 	$(hide) rm -rf $(WCNSS_BIN_SYMLINK)
- 	$(hide) ln -sf $(ACTUAL_BIN_FILE) $(WCNSS_BIN_SYMLINK)
- 	$(hide) rm -rf $(WCNSS_DAT_SYMLINK)
- 	$(hide) ln -sf $(ACTUAL_DAT_FILE) $(WCNSS_DAT_SYMLINK)
-	$(hide) rm -rf $(WCNSS_MODULE_SYMLINK)
-	$(hide) ln -sf $(ACTUAL_MODULE_FILE) $(WCNSS_MODULE_SYMLINK)
- 	$(hide) touch $@
+$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/Android.mk
+$(LOCAL_BUILT_MODULE):
+	$(hide) echo "Making symlinks for wifi"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) mkdir -p $(dir $(WCNSS_INI_SYMLINK))
+	$(hide) rm -rf $@
+	$(hide) rm -rf $(WCNSS_INI_SYMLINK)
+	$(hide) ln -sf $(ACTUAL_INI_FILE) $(WCNSS_INI_SYMLINK)
+	$(hide) rm -rf $(WCNSS_BIN_SYMLINK)
+	$(hide) ln -sf $(ACTUAL_BIN_FILE) $(WCNSS_BIN_SYMLINK)
+	$(hide) rm -rf $(WCNSS_DAT_SYMLINK)
+	$(hide) ln -sf $(ACTUAL_DAT_FILE) $(WCNSS_DAT_SYMLINK)
+	$(hide) touch $@
 
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
 
