eaf89e871b7b116930c9dadc5bc7c1ceb1f8a08e
Nikesh Kataria <nikkataria74@gmail.com>
2017-08-26 09:11:20 -0700

    Dex2oat crashing to take log

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,7 +127,7 @@ TARGET_HW_DISK_ENCRYPTION := true
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT := false
     endif
   endif
 endif
