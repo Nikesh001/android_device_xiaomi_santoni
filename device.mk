3b1911aa734f2839273bc5376f0afc1a7f2b3844
Nikesh001 <nikkataria74@gmail.com>
2017-11-15 10:16:24 +0530

    santoni: Build offline charger image
    Signed-off-by: Nikesh001 <nikkataria74@gmail.com>

--- device.mk
+++ device.mk
@@ -113,6 +113,10 @@ PRODUCT_PACKAGES += \
     libshim_camera \
     Snap
 
+# Offline Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8937 \
