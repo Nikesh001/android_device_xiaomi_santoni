2c6e723cf72955f6a514945b763b8b45b22d53c2
Nikesh001 <nikkataria74@gmail.com>
2017-11-20 17:32:17 +0530

    Fixed recent app switcher
    Finally recent app switcher fixed.
    Just need to give a proper name in keylayout to fix.
    Rename ft5x06_ts.kl --> ft5x06_720p.kl fixed recent app switcher.
    
    Signed-off-by: Nikesh001 <nikkataria74@gmail.com>

--- device.mk
+++ device.mk
@@ -164,9 +164,7 @@ PRODUCT_COPY_FILES += \
 
 # Input
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/Generic.kl:system/usr/keylayout/Generic.kl \
-    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
-    $(LOCAL_PATH)/keylayout/ft5435_ts.kl:system/usr/keylayout/ft5435_ts.kl \
+    $(LOCAL_PATH)/keylayout/ft5x06_720p.kl:system/usr/keylayout/ft5x06_720p.kl \
     $(LOCAL_PATH)/keylayout/gf3208.kl:system/usr/keylayout/gf3208.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl \
