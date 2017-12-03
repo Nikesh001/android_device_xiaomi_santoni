79cadcde8ef26a5cda4ec77f26d52152db84440d
Nikesh001 <nikkataria74@gmail.com>
2017-12-03 13:21:34 +0530

    santoni: CM >> LineageOS

--- lineage.mk
+++ lineage.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_santoni
 BOARD_VENDOR := Xiaomi
