a672c2a55d5557a94a7b290b68eadec9f0b6acc2
Paras Nagda <pnagda@codeaurora.org>
2017-12-15 13:28:14 +0530

    santoni: media_codecs.xml: load google's low end media codec xml
    load google's low end media codec xml for 8937
    
    Change-Id: Ifb924fa7823b422babfa67a6f7fe935a20de0f16

--- device.mk
+++ device.mk
@@ -282,7 +282,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/vendor/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/vendor/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/vendor/etc/media_codecs_google_video.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/vendor/etc/media_codecs_google_video_le.xml
 
 # Netutils
 PRODUCT_PACKAGES += \
