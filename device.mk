ce231ade91e505b274625b6cc4687831c08c0cad
Nikesh001 <nikkataria74@gmail.com>
2017-12-19 21:43:28 +0530

    santoni: Fixed Recent key
    fixed recent key by rename ft5x06_720p.kl--> ft5x06_ts.kl

--- device.mk
+++ device.mk
@@ -164,7 +164,7 @@ PRODUCT_COPY_FILES += \
 
 # Input
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/ft5x06_720p.kl:system/usr/keylayout/ft5x06_720p.kl \
+    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
     $(LOCAL_PATH)/keylayout/gf3208.kl:system/usr/keylayout/gf3208.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl \
