26f9e04f3f06a8fed9725755d990e405cc3bf5a3
Artem Borisov <dedsa2002@gmail.com>
2017-12-30 05:17:21 +0000

    santoni: Dex-preopt boot and system server
    WITH_DEXPREOPT_BOOT_IMG_ONLY was removed in 8.1 in favor of
    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY.
    
    Along with boot classpath it will also pre-optimize system server jars.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,11 +125,11 @@ TARGET_HW_DISK_ENCRYPTION := true
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := false
+      WITH_DEXPREOPT := true
     endif
   endif
 endif
-WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
 
 # Display
 MAX_VIRTUAL_DISPLAY_DIMENSION := 4096
