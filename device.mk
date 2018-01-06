f693deae62a4b4bc4e4860f6e49b988a1d7d58cd
Hriday Sharma <hridaysharma42@gmail.com>
2018-01-06 14:30:57 +0000

    santoni: Update seccomp policy

--- device.mk
+++ device.mk
@@ -339,7 +339,8 @@ PRODUCT_BOOT_JARS += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
 
 # Sensors
 PRODUCT_COPY_FILES += \
