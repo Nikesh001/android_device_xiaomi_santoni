7f7c69d6448590d2310420df7dcec0ba84d763ea
Joey <joey@lineageos.org>
2018-02-07 19:04:19 +0530

    santoni: Build textclassifier
    Change-Id: I50d8173d24c4cab773824107b98929dbbc469783

--- device.mk
+++ device.mk
@@ -389,6 +389,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+# TextClassifier smart selection model files
+PRODUCT_PACKAGES += \
+    textclassifier.smartselection.bundle1
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
