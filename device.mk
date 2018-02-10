91214e21321aab23f7eaa07d3f81e87bb3fdf79e
Nikesh001 <nikkataria74@gmail.com>
2018-02-10 06:58:05 +0000

    santoni: Switch to OSS RIL

--- device.mk
+++ device.mk
@@ -349,7 +349,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2 \
-    libprotobuf-cpp-full
+    libprotobuf-cpp-full \
+    libqdMetaData.system
 
 # Telephony
 PRODUCT_PACKAGES += \
