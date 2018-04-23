9831f0cd3ba90a0d0c18415231c3864cee90d7b5
Nikesh001 <nikkataria74@gmail.com>
2018-04-23 20:51:54 +0530

    santoni: use thermal-engine config from stock

--- device.mk
+++ device.mk
@@ -383,6 +383,9 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:system/vendor/etc/thermal-engine.conf
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
